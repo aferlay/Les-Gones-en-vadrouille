668e6edc19c7288c1c5e27a3aedcbc461f5f49fb
Msbarra <msbarra@etu.isara.fr>
2023-02-08 14:42:24 +0100

    Séparation de Ui et server

---
+++ MapLyon/server.R
@@ -0,0 +1,30 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  output$Carte <- renderLeaflet({
+    Carte <- leaflet() %>% addTiles()
+    Carte <- addMarkers(Carte, lng=4.84671 , lat=45.74846  , popup="Halles Paul Bocuse")
+    Carte <- addMarkers(Carte, lng=4.8275 , lat=45.7607 , popup="Cathédral Saint Jean")
+    Carte <- addMarkers(Carte, lng=4.8187 , lat=45.7337 , popup="Musée des Confluences")
+    Carte <- addMarkers(Carte, lng=4.85528 , lat=45.77737 , popup="Parc de la tête d'Or")
+    Carte <- addMarkers(Carte, lng=4.827787279578491 , lat=45.762048131221704 , popup="Musée de la miniature et du cinéma")
+    Carte <- addMarkers(Carte, lng=4.818496726 , lat=45.75583031 , popup="Théâtre romain de Fourvière")
+    Carte <- addMarkers(Carte, lng=4.833512 , lat=45.767272 , popup="Musée des Beaux-Arts ")
+    Carte <- addMarkers(Carte, lng=4.835791 , lat=45.758038  , popup="Musée de l'illusion")
+    Carte <- addMarkers(Carte, lng=4.821866 , lat=45.762361 , popup="Basilique de Fourvière")
+    Carte
+  })
+}
+
+shinyApp(ui = ui, server = server)
