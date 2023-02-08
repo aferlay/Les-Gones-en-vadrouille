ff11744fb632d3a56004ff1e2cea39690947d47f
Msbarra <msbarra@etu.isara.fr>
2023-02-08 13:13:30 +0100

    Notre dame de Fourvière

--- CarteAvecOnglets.R
+++ CarteAvecOnglets.R
@@ -17,11 +17,8 @@ ui <- fluidPage(
     ),
     #### MONUMENTS ####
     navbarMenu("Monuments",
-               tabPanel("NomMonument1",
-                        HTML('Vive les <u>pingouins</u> ! <br>
-               Ne pas confondre <b>pingouins</b> et <i>manchots</i>. <br>
-               Les pingouins sont &lt; ou &gt; aux manchots ? <br>
-               Les pingouins sont < ou > aux manchots ? <br>'
+               tabPanel("Notre dame de Fourvière",
+                        HTML('Notre-Dame de Fourvière est une basilique située sur la colline de Fourvière à Lyon. Elle a été construite entre 1872 et 1896 pour célébrer les événements religieux et les bénédictions sur la ville de Lyon. Cette basilique est considérée comme un symbole de la dévotion maritale en France et a été érigée en reconnaissance des grâces accordées à la ville pendant la peste de 1849. Le bâtiment a une architecture néogothique unique et est considéré comme l’un des plus beaux monuments religieux de France. Il est également célébré pour son panorama sur la ville de Lyon et les Alpes. Environ 4 millions de personnes visitent Notre-Dame de Fourvière chaque année, ce qui en fait l’un des sites touristiques les plus populaires de la région lyonnaise. De plus, le nombre de visiteurs est en continuelle expansion.'
                         )
                ),
                tabPanel("NomMonument2",
