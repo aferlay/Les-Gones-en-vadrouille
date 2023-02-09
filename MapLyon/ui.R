78fa9eb1840bb9c01b31407b39af5f55c3383d1e
lebrtheo <124247731+lebrtheo@users.noreply.github.com>
2023-02-09 11:30:05 +0100

    Update ui.R

--- MapLyon/ui.R
+++ MapLyon/ui.R
@@ -84,35 +84,69 @@ fluidPage(
                ),
                ##### Tête d'or #####
                tabPanel("Parc de la tête d’or",
-                        HTML('Le Parc de la Tête d Or, situé à Lyon en France, est un vaste parc public de 117 hectares accessible par 8 entrées, considéré comme l un des plus grands parcs urbains de France. Le parc a été créé en 1857 sous Napoléon III et l Impératrice Eugénie, comme un espace public pour la population lyonnaise.
-Il a été conçu par le paysagiste Jean-Charles Alphand, qui a également travaillé sur les parcs de Bois de Boulogne à Paris et de La Canebière à Marseille. Il est réputé pour ses jardins paysagers, ses étangs, ses allées ombragées et ses installations de loisirs pour des activités en plein air. Le parc comprend également un zoo, un jardin botanique, une roseraie, un petit train touristique et une plage artificielle.
-Celui-ci est composé de nombreux bâtiments comme les grandes serres, le vélodrome, le chalet des gardes et le chalet du parc, la clôture, les serres de collection, le monument aux morts de l île aux Cygnes ou encore la nouvelle roseraie.
-C est un endroit de détente populaire pour les habitants de Lyon, où ils peuvent pique-niquer, se détendre sur les pelouses, faire du jogging, du vélo, etc. Le Parc de la Tête d Or est un incontournable pour les visiteurs de Lyon, offrant un espace vert paisible en plein cœur de la ville.
+                        HTML('Le Parc de la Tête d Or, situé à Lyon en France, est un vaste parc public de 117 hectares accessible par 8 entrées, 
+                              considéré comme l un des plus grands parcs urbains de France. 
+                              Le parc a été créé en 1857 sous Napoléon III et l Impératrice Eugénie, comme un espace public pour la population lyonnaise.
+                              Il a été conçu par le paysagiste Jean-Charles Alphand, qui a également travaillé sur les parcs de Bois de Boulogne à Paris 
+                              et de La Canebière à Marseille. Il est réputé pour ses jardins paysagers, ses étangs, ses allées ombragées et ses installations 
+                              de loisirs pour des activités en plein air. Le parc comprend également un zoo, un jardin botanique, une roseraie, 
+                              un petit train touristique et une plage artificielle.
+                              Celui-ci est composé de nombreux bâtiments comme les grandes serres, le vélodrome, le chalet des gardes et le chalet du parc, 
+                              la clôture, les serres de collection, le monument aux morts de l île aux Cygnes ou encore la nouvelle roseraie.
+                              C est un endroit de détente populaire pour les habitants de Lyon, où ils peuvent pique-niquer, se détendre sur les pelouses, 
+                              faire du jogging, du vélo, etc. Le Parc de la Tête d Or est un incontournable pour les visiteurs de Lyon, 
+                              offrant un espace vert paisible en plein cœur de la ville.
 '
                         )
                ),
                ##### Confluences #####
                tabPanel("Musée des confluences",
-                        HTML('Le Musée des Confluences, situé dans le quartier de La Confluence à Lyon en France, est un établissement scientifique et culturel qui a ouvert ses portes en 2014, après plus de dix ans de travail et de préparation. Il se concentre sur la présentation de l histoire de l humanité, de la science et de la technologie, ainsi que sur les défis sociaux actuels.
-Le bâtiment du Musée des Confluences a été conçu par l architecte Jean-Paul Viguier et a une forme distinctive en forme de boîte d œuf, située à la confluence du Rhône et de la Saône.
-Le musée propose des expositions permanentes et temporaires, des activités éducatives pour tous les âges et une variété de programmes culturels. La collection permanente du musée comprend des artefacts scientifiques, historiques et culturels provenant du monde entier.
-Le Musée des Confluences est considéré comme un important centre culturel et scientifique, offrant des activités éducatives pour les enfants et les adultes, ainsi qu une programmation culturelle variée. Depuis son ouverture, le musée a attiré des milliers de visiteurs chaque année et est devenu un lieu incontournable pour ceux qui cherchent à en savoir plus sur l histoire, la science et la société.
+                        HTML('Le Musée des Confluences, situé dans le quartier de La Confluence à Lyon en France, est un établissement scientifique 
+                              et culturel qui a ouvert ses portes en 2014, après plus de dix ans de travail et de préparation. 
+                              Il se concentre sur la présentation de l histoire de l humanité, de la science et de la technologie, 
+                              ainsi que sur les défis sociaux actuels.
+                              Le bâtiment du Musée des Confluences a été conçu par l architecte Jean-Paul Viguier et a une forme distinctive en forme de boîte d œuf, 
+                              située à la confluence du Rhône et de la Saône.
+                              Le musée propose des expositions permanentes et temporaires, des activités éducatives pour tous les âges 
+                              et une variété de programmes culturels. La collection permanente du musée comprend des artefacts scientifiques, 
+                              historiques et culturels provenant du monde entier.
+                              Le Musée des Confluences est considéré comme un important centre culturel et scientifique, offrant des activités éducatives 
+                              pour les enfants et les adultes, ainsi qu une programmation culturelle variée. 
+                              Depuis son ouverture, le musée a attiré des milliers de visiteurs chaque année et est devenu un lieu incontournable pour ceux 
+                              qui cherchent à en savoir plus sur l histoire, la science et la société.
 '
                         )
                ),
                ##### Miniature&Cinéma #####
                tabPanel("Musée de la miniature et du cinéma",
-                        HTML('Le Musée de la miniature et du cinéma a été créé en 2005 en raison du travail de l artiste miniaturiste nommé Dan Ohlmann. Dans le musée sont exposées deux collections permanentes: une partie consacrée à l art de la miniature et l autre partie, le cinéma. La partie des miniatures a plus de 100 maquettes réalistes qui représentent des scènes du quotidien. Ces œuvres ont été réalisées par divers artistes de l ébénisterie en passant par la maroquinerie, l orfèvrerie ou encore la lutherie. La partie dédiée au cinéma réunit toutes types de décors, costumes, masques ou prothèses utilisées en grands films. Le musée a aussi un support explicatif sur la réalisation des effets spéciaux au cinéma et comporte, également, des robots et des monstres ainsi que toute type de masques qui permettent de vieillir ou de rajeunir les acteurs.'
+                        HTML('Le Musée de la miniature et du cinéma a été créé en 2005 en raison du travail de l artiste miniaturiste nommé Dan Ohlmann. 
+                              Dans le musée sont exposées deux collections permanentes: une partie consacrée à l art de la miniature et l autre partie, le cinéma. 
+                              La partie des miniatures a plus de 100 maquettes réalistes qui représentent des scènes du quotidien. 
+                              Ces œuvres ont été réalisées par divers artistes de l ébénisterie en passant par la maroquinerie, l orfèvrerie ou encore la lutherie. 
+                              La partie dédiée au cinéma réunit toutes types de décors, costumes, masques ou prothèses utilisées en grands films. 
+                              Le musée a aussi un support explicatif sur la réalisation des effets spéciaux au cinéma et comporte, également, 
+                              des robots et des monstres ainsi que toute type de masques qui permettent de vieillir ou de rajeunir les acteurs.'
                         )
                ),
                ##### Théatre Romain #####
                tabPanel("Théâtre romain de Fourvière",
-                        HTML('Le théâtre de Lugdunum est un site archéologique remarquable, emblématique de Lyon antique et un des principaux monuments romains existants. Il a été construit au début de l Empire, possiblement dans le règne d Auguste, et peut accueillir jusqu à 10 000 spectateurs, ce monument a été abandonné à la fin de l Empire romain et transformé en carrière complètement enseveli jusqu à au Moyennage. Le site archéologique a été dégagé au début du 20ème siècle et est inscrit sur la liste du patrimoine mondial de l Unesco.'
+                        HTML('Le théâtre de Lugdunum est un site archéologique remarquable, emblématique de Lyon antique et 
+                              un des principaux monuments romains existants. Il a été construit au début de l Empire, possiblement dans le règne d Auguste, 
+                              et peut accueillir jusqu à 10 000 spectateurs, ce monument a été abandonné à la fin de l Empire romain 
+                              et transformé en carrière complètement enseveli jusqu à au Moyennage. 
+                              Le site archéologique a été dégagé au début du 20ème siècle et est inscrit sur la liste du patrimoine mondial de l Unesco.'
                         )
                ),
                ##### Beaux-arts #####
                tabPanel("Musée des beaux-arts",
-                        HTML('Le musée des Beaux-Arts de Lyon est localisé dans l abbaye des Dames de Saint-Pierre, une bâtiment du 17ème siècle situé dans la région historique de la ville de Lyon. Le musée est ouvert au public depuis 1803 et, en 1998, a été entièrement rénové. Actuellement le musée a l une des plus importantes collections d oeuvres d art d Europe en cinq départements et 70 salles répartis sur plus de 7.000 m² : le département des Antiquités avec l histoire des grandes civilisations antiques du Bassin Méditerranéen. Le département des Objets d art et des sculptures mêlent les arts précieux et la sculpture, du Moyen-âge à la période Art Déco.Le Médailler du musée est l un des plus importants de France avec plus de 50 000 monnaies, médailles, sceaux et plombs. Les sculptures des 19ème et 20ème et la collection de peintures depuis le 14ème siècle jusqu aux années 1980. Toute l année, le musée des Beaux-Arts propose aussi des grandes expositions et des activités culturelles.'
+                        HTML('Le musée des Beaux-Arts de Lyon est localisé dans l abbaye des Dames de Saint-Pierre, une bâtiment du 17ème siècle 
+                              situé dans la région historique de la ville de Lyon. Le musée est ouvert au public depuis 1803 et, en 1998, a été entièrement rénové. 
+                              Actuellement le musée a l une des plus importantes collections d oeuvres d art d Europe en cinq départements 
+                              et 70 salles répartis sur plus de 7.000 m² : le département des Antiquités avec l histoire des grandes civilisations antiques 
+                              du Bassin Méditerranéen. Le département des Objets d art et des sculptures mêlent les arts précieux et la sculpture, 
+                              du Moyen-âge à la période Art Déco.Le Médailler du musée est l un des plus importants de France avec plus de 50 000 monnaies, 
+                              médailles, sceaux et plombs. Les sculptures des 19ème et 20ème et la collection de peintures depuis le 14ème siècle 
+                              jusqu aux années 1980. Toute l année, le musée des Beaux-Arts propose aussi des grandes expositions et des activités culturelles.'
                         )
                )
     )
